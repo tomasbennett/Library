f7cbbdf9c0a7dda9ae1fc9768ba0ee723a405346
tomasbennett <tjs.bennett@gmail.com>
2025-02-15 23:11:27 +0000

    Correcting issues with the heights of the libraries example books as well as removing some of the responsiveness to the web page as it required further work for mobile users that I will be working on finishing next

--- scripts/addBook.js
+++
@@ -1,9 +0,0 @@
-const svgElem = document.getElementById("add-book");
-
-svgElem.addEventListener("mousedown", addBook);
-
-function addBook(){
-    const dialogBox = document.getElementById("dialog");
-
-    dialogBox.showModal();
-}
