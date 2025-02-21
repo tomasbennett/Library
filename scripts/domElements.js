a01338050a88a61f93a1c65e20e12436677e765e
tomasbennett <tjs.bennett@gmail.com>
2025-02-21 20:47:49 +0000

    Adding prototype functionality to localStorage.js without adding the image specification from imageCarousel.js

--- scripts/domElements.js
+++ scripts/domElements.js
@@ -1,14 +1,15 @@
 export const svgAddBook = document.getElementById('add-book');
 export const pathAddBook = document.getElementById('path6');
 
+export const dialog = document.getElementById('dialog');
 export const dialogForm = document.getElementById('dialog-form');
-export const dialogTitleInput = document.getElementById('');
-export const dialogDescriptionInput = document.getElementById('');
-export const dialogPrevReadInput = document.getElementById('');
-
+export const dialogTitleInput = document.getElementById('title-input');
+export const dialogDescriptionInput = document.getElementById('description-input');
+export const dialogPrevReadInput = document.getElementById('prev-read-input');
 export const dialogFormSubmitBtn = document.getElementById('submit-book-edit');
-export const booksContainer = document.getElementById('books-container');
 
 export const imageContainer = document.getElementById('image-carousel-container');
 export const svgLeftArrow = document.getElementById('left-arrow-svg');
 export const svgRightArrow = document.getElementById('right-arrow-svg');
+
+export const booksContainer = document.getElementById('books-container');
